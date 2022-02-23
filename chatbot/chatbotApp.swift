e4a48f1ac14b197485d8de2c066b00b199bcbd66
takata <skkyosk1124@gmail.com>
2022-02-24 08:20:23 +0900

    Initial Commit

---
+++ chatbot/chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbotApp.swift
+//  chatbot
+//
+//  Created by t032fj on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
